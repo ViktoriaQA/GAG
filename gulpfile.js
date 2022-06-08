11e299d5d9ca9f296a719113d2444862d7d772c7
qa <eboko1@gmail.com>
2022-06-08 11:29:04 +0300

    added include html files

--- gulpfile.js
+++ gulpfile.js
@@ -9,11 +9,14 @@ global.app = {
 
 import {copy} from './gulp/tasks/copy.js';
 import {reset} from './gulp/tasks/reset.js';
+import {html} from './gulp/tasks/html.js';
 
 function watcher (){
     gulp.watch(path.watch.files, copy)
+    gulp.watch(path.watch.html, copy)
 }
+const mainTask = gulp.parallel(copy, html) // копіювання html файлів та інших файлів
 
-const dev = gulp.series(reset, copy, watcher); // виконує задачі послідовно (reset на поч. щоб очищало папку dist потім copy)
+const dev = gulp.series(reset, mainTask, watcher); // виконує задачі послідовно (reset на поч. щоб очищало папку dist потім copy)
 
 gulp.task('default', dev)
